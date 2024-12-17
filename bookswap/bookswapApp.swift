686ef7d8313bd7d508d0088c23eb893511902dc5
saniash926 <saniash926@gamil.com>
2024-12-17 15:00:42 +0100

    Initial Commit

---
+++ bookswap/bookswapApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookswapApp.swift
+//  bookswap
+//
+//  Created by Sania Shapasand on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct bookswapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
